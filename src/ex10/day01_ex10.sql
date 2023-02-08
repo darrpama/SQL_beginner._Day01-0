410702fef981b80da41faa27e09233cd79572abf
darrpama <darrpama@student.21-school.ru>
2023-02-08 18:14:40 +0300

    done this

---
+++ src/ex10/day01_ex10.sql
@@ -0,0 +1,6 @@
+SELECT person.name as person_name, menu.pizza_name AS pizza_name, pizzeria.name AS pizzeria_name
+FROM person
+JOIN person_order ON person.id = person_order.person_id
+JOIN menu ON menu.id = person_order.menu_id
+JOIN pizzeria ON menu.pizzeria_id = pizzeria_id
+ORDER BY person_name, pizza_name, pizzeria_name;
